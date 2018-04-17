254dc45b0625af9cebe07bcfd72396229e64eb7a
s9alfloh <s9alfloh@stud.uni-saarland.de>
2018-04-18 00:26:18 +0200

    DegreeDistribution.py done

--- Assign1/DegreeDistribution.py
+++ Assign1/DegreeDistribution.py
@@ -1,3 +1,4 @@
+import AbstractNetwork
 class DegreeDistribution:
     """Calculates a degree distribution for a network"""
 
@@ -5,8 +6,14 @@ class DegreeDistribution:
         """
         Inits DegreeDistribution with a network and calculate its distribution
         """
-    
+	self.histogram = [0] * network.maxDegree()
+	for i in range(0, network.size()):
+		self.histogram[network.getNode(i).degree()] += 1
+    	getNormalizedDistribution()
+
     def getNormalizedDistribution(self):
         '''
         Returns the computed normalized distribution
         '''
+	for i in range(0, network.maxDegree()):
+		self.histogram /= network.size()
---
+++ Assign1/test.py
@@ -0,0 +1,10 @@
+from Node import Node
+from AbstractNetwork import AbstractNetwork 
+from RandomNetwork import RandomNetwork 
+#from AbstractNetwork import RandomNetwork 
+from DegreeDistribution import DegreeDistribution
+
+absnet = RandomNetwork(5, 10)
+
+
+
