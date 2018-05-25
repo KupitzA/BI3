6dc2286701afdb9bc512ccaede3aacbd4f906855
AlexanderFlohr <alex.flohr@web.de>
2018-05-25 11:25:04 +0200

    Final

--- Assign5/exercise5_2.py
+++ Assign5/exercise5_2.py
@@ -2,5 +2,5 @@ from PPIGONetwork import PPIGONetwork
 
 # 5.2 (a) (1)
 print "Start program"
-net = PPIGONetwork("human_network.tsv", "human_uniprot.tsv", "human_GO.gaf")
+net = PPIGONetwork("pig_network.tsv", "pig_uniprot.tsv", "pig_GO.gaf")
 net.overview()
