0c7178cf336b074b728f4123fd092df450c6c968
Andrea Kupitz <s9ankupi@bio11.studcs.uni-saarland.de>
2018-06-06 14:56:08 +0200

    task1

--- Assign7/imput.R
+++ Assign7/imput.R
@@ -14,10 +14,21 @@ sd <- sd(DTable)
 # Derive the new mean and standard deviation for the missing data based on the current
 # distribution
 nansize <- dim(DTable0)[1]-dim(DTable)[1]
-lowquan <- qnorm(0.25)
-lowdata <- subset(DTable, DTable[,1] <= mean+lowquan)
+lowquan <- qnorm(0.25, mean, sd)
+lowdata <- subset(DTable, DTable[,1] <= lowquan)
 newmean <- mean(lowdata)
 newsd <- sd(lowdata)
 
 # Generate the new data based on the new mean and standard deviation from the previous step
 imputdata <- rnorm(nansize, newmean, newsd)
+
+hist1 <- hist(DTable, col="blue")
+hist2 <- hist(imputdata, add=T, col="red")
+#plot()
+for(mean in seq(from = 20, to = lowquan, by = 2)){
+  for(sd in seq(from = 1, to = sd, by = 0.2)){
+    imputdata1 <- rnorm(nansize, mean, sd)
+    hist3 <- hist(DTable, col="blue")
+    hist4 <- hist(imputdata1, add=T, col="red")
+  }
+}
