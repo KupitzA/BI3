a28d2d2d37a9ee3049621fa37fbc0c2e13dbe17c
AlexFlohr <alex.flohr@web.de>
2018-06-12 01:34:09 +0200

    Some bug - kills to many links

--- Assign7/ex_7_2.R
+++ Assign7/ex_7_2.R
@@ -13,7 +13,7 @@ variance <- rep(0, ncol(mtrx))
 # global container storing the probabilities
 mtrx_prob <- mtrx[2:nrow(mtrx), ]
 
-# update varance from wildtype row
+# initialize genes varances for mutants from wildtype
 initializeVariance <- function() {
   for (j in 1:ncol(mtrx)) {
     temp <- 0
@@ -25,6 +25,10 @@ initializeVariance <- function() {
   }
 }
 
+# update the variances for each gene based on the
+# equation of noise modle step 2
+# since we previousely used variances for each gene
+# we interpret the equation in the same way
 updateVariance <- function(p_from, p_to) {
   curr_variance <- rep(0, ncol(mtrx))
   counter <- rep(0, ncol(mtrx))
@@ -36,14 +40,16 @@ updateVariance <- function(p_from, p_to) {
   }
   for(i in 1:length(curr_variance)){
     if(counter[i] != 0){
-      variance[i] <<- curr_variance[i]/counter[i]
+      variance[i] <<- curr_variance[i]/(1+counter[i])
     }
   }
 }
 
+# compute the probabilities that the deviation is due
+# to a real regulation event
 probabilities <- function() {
-  for (b in 1:ncol(mtrx)) {
-    for (a in 1:ncol(mtrx)) {
+  for (b in 1:nrow(mtrx_prob)) {
+    for (a in 1:ncol(mtrx_prob)) {
       deviation <- abs(mtrx[b + 1, a] - mtrx[1, a])
       phi <- pnorm(deviation / sqrt(variance[a]))
       mtrx_prob[b, a] <<- 2 * phi - 1
@@ -51,6 +57,8 @@ probabilities <- function() {
   }
 }
 
+# re-estimate wilde-type expression level
+# accounts to step 3 of the noise modle
 updateWildType <- function(p_from, p_to){
   for(i in 1:ncol(mtrx)){
     temp <- mtrx[1,i]
@@ -65,21 +73,23 @@ updateWildType <- function(p_from, p_to){
   }
 }
 
+
 refine <- function() {
   # Noise model iterative step (1)
+  # search gene pairs for refining the error modle
   p_from <<- c()
   p_to <<- c()
-  for (b in 1:ncol(mtrx)) {
-    for (a in 1:ncol(mtrx)) {
+  for (b in 1:nrow(mtrx_prob)) {
+    for (a in 1:ncol(mtrx_prob)) {
       if (mtrx_prob[b, a] < 0.05) {
-        p_from <<- c(p_from, b)
-        p_to <<- c(p_to, a)
+        p_from <- c(p_from, b)
+        p_to <- c(p_to, a)
       }
     }
   }
   # Noise model iterative step (2)
   updateVariance(p_from, p_to)
-  # Noise model iterative step (2)
+  # Noise model iterative step (3)
   updateWildType(p_from, p_to)
 }
 
@@ -87,24 +97,22 @@ output <- function(){
   for (b in 1:nrow(mtrx_prob)) {
     for (a in 1:ncol(mtrx_prob)) {
       if (mtrx_prob[b, a] < 0.05) {
-        cat(b, a, mtrx_prob[b,a], "\n")
+        cat(colnames(mtrx)[b], "\t",
+            colnames(mtrx)[a], "\t",
+            mtrx_prob[b,a], "\n")
       }
     }
   }
-  
-  
 }
 
-
-
 # Main
-iterations <- 5
-
+iterations <- 1
 initializeVariance()
-probabilities()
 
 for (i in 1:iterations) {
-  refine()
+  # First part of Noise model step (1)
+  # calculate probabilities for each pair of genes
   probabilities()
+  refine()
 }
 output()
